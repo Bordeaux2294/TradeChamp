6305a19bb0c341c2c9feff2e4c200f3795a53313
AllahDalla <alromariodavis@gmail.com>
2024-09-14 16:44:15 -0500

    Tested API CORS with fetch call

--- client/tradechamp/src/App.js
+++ client/tradechamp/src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+
+  // testing api request -> REMOVE WHEN READY FOR DEVELOPMENT
+
+  useEffect(()=>{
+    async function test(){
+      try {
+        const response = await fetch('http://localhost:1738/test');
+        if(response.ok){
+          console.log(await response.text())
+        }else{
+          console.log("RESPONSE WAS NOT OK")
+        }
+      } catch (error) {
+        console.log("AN ERROR OCCURED -> ", error)
+      }
+    }
+    test()
+  }, )
+
+
+
   return (
     <div className="App">
       <header className="App-header">
